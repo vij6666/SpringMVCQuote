9c98aa2f811a0a050fca4e812ea63e5c3e69b097
Vijay Sidhu <vijaysidhu.vs@gmail.com>
2017-01-31 19:39:57 +0000

    Initial Commit

---
+++ src/main/java/com/vijay/SpringMvcQuoteApplication.java
@@ -0,0 +1,12 @@
+package com.vijay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcQuoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcQuoteApplication.class, args);
+	}
+}
