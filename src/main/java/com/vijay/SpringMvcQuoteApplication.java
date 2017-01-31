ebc296e270cb1f0fd2cd21ea01acaefe62cae8d3
Vijay Sidhu <vijaysidhu.vs@gmail.com>
2017-01-31 23:51:37 +0000

    Push of wro4j config to build custom css and js files and index html

--- src/main/java/com/vijay/SpringMvcQuoteApplication.java
+++ src/main/java/com/vijay/SpringMvcQuoteApplication.java
@@ -2,12 +2,15 @@ package com.vijay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringMvcQuoteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcQuoteApplication.class, args);
-		System.out.println("HelloWorld!");
+	
 	}
 }
